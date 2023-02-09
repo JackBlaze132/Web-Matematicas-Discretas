35cb91fa841ebae1ab0710af40c88a978715b703
ST2712 <111332821+ST2712@users.noreply.github.com>
2023-02-08 20:24:04 -0500

    first commit

---
+++ src/main/java/com/co/nodos/MD_PPCTM/MdPpctmApplication.java
@@ -0,0 +1,13 @@
+package com.co.nodos.MD_PPCTM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdPpctmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdPpctmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/nodos/MD_PPCTM/MdPpctmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.nodos.MD_PPCTM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdPpctmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
