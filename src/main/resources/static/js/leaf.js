991827043afd7ea48afb94f7627fb4b7765fa70a
JackBlaze132 <63567815+JackBlaze132@users.noreply.github.com>
2023-04-18 19:35:30 -0500

    Pequeña correción en la libreria leaf.js

--- src/main/resources/static/js/leaf.js
+++ src/main/resources/static/js/leaf.js
@@ -9,12 +9,18 @@ let leaves = document.querySelectorAll(".leaf")
 for ( let i = 0 ; i < fathers.length; i++){
     for (let j = 0 ; j < leaves.length; j++){
 
+        /*
+        console.log("Primer entry")
         console.log(leaves[j].id)
         console.log(fathers[i].classList)
+        */
 
         if (fathers[i].classList.contains(String(leaves[j].id))){
+            /*
+            console.log("Segundo entry")
             console.log(fathers[i].id)
             console.log(leaves[j].id)
+            */
             LineController.drawLine({
                 left_node: String(fathers[i].id),
                 right_node: String(leaves[j].id),
